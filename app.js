d32a631f588b401767e3f4291ede71c4968c8c5a
marioosh <marioosh@marioosh.net>
2021-11-02 09:01:41 +0100

    default port: 3006

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto');
 const http = require('http');
-const port = process.env.PORT?process.env.PORT:3000;
+const port = process.env.PORT?process.env.PORT:3006;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
